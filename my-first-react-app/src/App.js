34b2b707b942aa2b49c7535a91e5382f9a3af341
Subrahmanyam956 <subrahmanyam956@gmail.com>
2025-06-28 21:50:48 +0530

    first react app

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello! 👋 I'm Subrahmanyam</h1>
+      <p>
+        I'm a budding React developer learning to build modern web apps. I enjoy
+        learning new things and solving real-world problems.
+      </p>
+
+      <p>
+         I am learing React, Just so that I can use React to develop simple FrontEnd apps and become a 
+        Full stack engineer when opportunity comes.
+      </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
